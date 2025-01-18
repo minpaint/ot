259e0faa33f73f13fac8e19132898bda97f71c7c
Alexander <min7035911@gmail.com>
2025-01-18 11:50:11 +0300

    Новый проект

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.organizations.views import OrganizationViewSet
+
 from apps.employees.views import EmployeeViewSet
+from apps.organizations.views import OrganizationViewSet
+from apps.departments.views import DepartmentViewSet
+from apps.positions.views import PositionViewSet
 from apps.documents.views import DocumentViewSet
-from apps.importer.views import ImportSessionViewSet
 
 router = DefaultRouter()
-router.register(r'organizations', OrganizationViewSet)
-router.register(r'employees', EmployeeViewSet)
-router.register(r'documents', DocumentViewSet)
-router.register(r'import', ImportSessionViewSet)
-
-app_name = 'api'
+router.register('employees', EmployeeViewSet, basename='employees')
+router.register('organizations', OrganizationViewSet, basename='organizations')
+router.register('departments', DepartmentViewSet, basename='departments')
+router.register('positions', PositionViewSet, basename='positions')
+router.register('documents', DocumentViewSet, basename='documents')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/', include('rest_framework.urls')),
 ]
