82f68cd5b3557934a6a043dba4f7c6695965f19e
minpaint <MINPAINT@yandex.by>
2025-01-19 22:04:34 +0300

    Delete config directory

--- config/settings/production.py
+++
@@ -1,9 +0,0 @@
-from .base import *
-
-DEBUG = False
-
-# Настройки безопасности
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_BROWSER_XSS_FILTER = True
